74e5e4f937c50fdb1365e1ff90b5b1d5d6a08c15
boraerinc <bora17erinc@gmail.com>
2025-06-07 16:55:43 +0100

    .

--- src/lib/solana.ts
+++ src/lib/solana.ts
@@ -5,14 +5,10 @@ import {
   CoinbaseWalletAdapter
 } from '@solana/wallet-adapter-wallets'
 import { clusterApiUrl } from '@solana/web3.js'
-import { MetaMaskSolanaWalletAdapter } from './metamask-solana-adapter'
 
 export const network = WalletAdapterNetwork.Mainnet
 export const endpoint = clusterApiUrl(network)
 
-// Main Solana wallets including support for MetaMask native Solana
-// Note: MetaMask with native Solana support should be automatically detected
-// by the wallet adapter system and appear in the wallet list
 export const wallets = [
   new PhantomWalletAdapter({ network }),
   new SolflareWalletAdapter({ network }),
