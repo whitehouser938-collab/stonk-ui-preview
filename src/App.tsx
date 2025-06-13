9e5bef5714a7db990b8296a8467d58faf45a218e
boraerinc <bora17erinc@gmail.com>
2025-06-13 14:18:00 +0100

    Implement Reown Wallet Connect (Desktop only)

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { WagmiProvider } from 'wagmi';
-import { config } from '@/lib/wagmi';
-import { SolanaProvider } from "@/components/SolanaProvider";
 import Index from "./pages/Index";
+
+//Reown imports
+import { createAppKit } from '@reown/appkit/react'
+import { arbitrum, mainnet, base, sepolia, AppKitNetwork, solana, solanaTestnet, solanaDevnet, polygon } from '@reown/appkit/networks'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { SolanaAdapter } from '@reown/appkit-adapter-solana/react'
+
 import Markets from "./pages/Markets";
 import Research from "./pages/Research";
 import Launchpad from "./pages/Launchpad";
@@ -14,10 +19,40 @@ import Leaderboard from "./pages/Leaderboard";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID;
+
+if (!projectId) {
+  throw new Error('VITE_WALLETCONNECT_PROJECT_ID is not set');
+}
+
+const metadata = {
+  name: 'Stonk Terminal',
+  description: 'Stonk Terminal',
+  url: window.location.origin,
+  icons: ['https://stonkterminal.com/favicon.ico']
+}
+
+const networks = [mainnet, arbitrum, base, sepolia, polygon, solana, solanaTestnet, solanaDevnet] as [AppKitNetwork, ...AppKitNetwork[]];
+
+const solanaWeb3JsAdapter = new SolanaAdapter();
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr:true,
+})
+
+const appKit = createAppKit({
+  adapters: [wagmiAdapter, solanaWeb3JsAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true
+  }
+})
 
 const App = () => (
-  <WagmiProvider config={config}>
-    <SolanaProvider>
+  <WagmiProvider config={wagmiAdapter.wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <Toaster />
@@ -34,7 +69,6 @@ const App = () => (
           </BrowserRouter>
         </TooltipProvider>
       </QueryClientProvider>
-    </SolanaProvider>
   </WagmiProvider>
 );
 
